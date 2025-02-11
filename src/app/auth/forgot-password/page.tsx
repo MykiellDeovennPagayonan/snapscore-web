99ab5600892016dde36f7f88aafd8e9efdeafcbf
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2025-02-11 21:57:02 +0800

    [Add] Forget password

---
+++ src/app/auth/forgot-password/page.tsx
@@ -0,0 +1,10 @@
+import ForgotPassword from "@/components/auth/ForgetPassword"
+
+export default function Page() {
+
+  return (
+    <div className="w-full flex flex-col">
+      <ForgotPassword />
+    </div>
+  )
+}
